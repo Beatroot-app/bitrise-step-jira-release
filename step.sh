ee03428581f54a4bf68c16ac97809e1d8147eddd
ydemetriades <yiannis.demetriades@gmail.com>
2022-03-08 13:36:13 +0200

    update step and script

--- step.sh
+++ step.sh
@@ -6,12 +6,29 @@ pip install --no-cache-dir requests argparse
 
 echo "Executing jira-release"
 
+COMMAND=$(cat << EOF
 ./jira-release.py \
     -v ${version_name} \
     -p ${projectId} \
     -u ${user} \
     --password ${password} \
     --update ${update} \
-    --released ${released} \
-    --archived ${archived} \
     --url ${url}
+EOF
+)
+
+if [ ! -z "${new_version}" -a "${new_version}" != " " ]; then
+    COMMAND="$COMMAND --new-version ${new_version}"
+fi
+
+if [ ! -z "${description}" -a "${description}" != " " ]; then
+    COMMAND="$COMMAND --description ${description}"
+fi
+
+if [ "${released}" != "Ignore" ] && [ ! -z "${new_version}" -a "${new_version}" != " " ]; then
+    COMMAND="$COMMAND --released ${released}"
+fi
+
+if [ "${archived}" != "Ignore" ] && [ ! -z "${new_version}" -a "${new_version}" != " " ]; then
+    COMMAND="$COMMAND --archived ${archived}"
+fi
